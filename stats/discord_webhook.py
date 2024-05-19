cbe6432817d283953c5427f9e7b774a213928cfc
Hugo^3 <pondypondo@outlook.com>
2024-05-19 23:43:31 +0100

    Initial commit

--- stats/discord_webhook.py
+++ stats/discord_webhook.py
@@ -1,18 +1,36 @@
-from base64 import b64encode
-import requests
-import traceback
 import os
 import time
+import requests
+import traceback
+from functools import partial
 
-# config
-WEBHOOK_URL = ""
-IMGBB_KEY = ""
+from base64 import b64encode
+
+# load config
+try:
+    with open("webhook.config", 'r', encoding="utf-8") as f:
+        WEBHOOK_URL = f.readline().strip()
+        IMGBB_KEY = f.readline().strip()
+    print("Successfully loaded webhook and imgbb api")
+except:
+    WEBHOOK_URL = "***REMOVED***"
+    IMGBB_KEY = "***REMOVED***"
+print(f"Loaded API:\nDiscord Webhook: {WEBHOOK_URL}\nImgBB: {IMGBB_KEY}")
 
 if not os.path.exists("history"):
     os.mkdir("history")
 
+def try_upload(f: partial, max_retry: int=3, wait_time: float=0.5, msg: str="Failed"):
+    for _ in [0] * max_retry:
+        try:
+            return f()
+        except:
+            time.sleep(wait_time)
+    raise Exception(msg)
+
 class discord_bot:
-    """A discord bot class that utilizes discord webhook to send stats and plots during training 
+    """
+    A discord bot class that utilizes discord webhook to send stats and plots during training 
     """    
     def __init__(self, url: str = WEBHOOK_URL, extra: str=''):
         current_time = time.strftime("%y-%m-%d-%H-%M-%S")
@@ -39,6 +57,7 @@ class discord_bot:
 
     def send_img(self, epoch_num: int):
         self.last_upload = []
+        epoch_name = epoch_num + 1 if isinstance(epoch_num, int) else str(epoch_num)
         
         with open(f"{self.path}/current.png", "rb") as file:
             url = "https://api.imgbb.com/1/upload"
@@ -49,16 +68,19 @@ class discord_bot:
             }
             
         try:
-            result = requests.post(url, payload)
+            f = partial(requests.post, url, payload)
+            result = try_upload(f, msg=f"Unable to upload images for epoch {epoch_name}")
             self.last_upload.append(result.json()["data"]["display_url"])
         except:
-            print(f"Unable to upload images for epoch {epoch_num+1}")
+            print(f"Unable to upload images for epoch {epoch_name}")
+            return
             
         try:
-            self.update_data_img(epoch_num)
-            requests.post(self.url, json = self.data)
+            self.update_data_img(epoch_name)
+            f = partial(requests.post, self.url, json=self.data)
+            result = try_upload(f, msg=f"Unable to send images for epoch {epoch_name}")
         except:
-            print(f"Unable to send images for epoch {epoch_num+1}")
+            print(f"Unable to send images for epoch {epoch_name}")
     
     def send_string(self, content: str):
         stat_data = {
