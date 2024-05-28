dc988431ca66e1a498c2f738a4b69f503b015b93
Hugo^3 <pondypondo@outlook.com>
2024-05-28 02:15:50 +0100

    Small adjustments to gather more data when playing as human player

--- playit.py
+++ playit.py
@@ -1,6 +1,8 @@
 import vizdoom as vzd
 import os
+import numpy as np
 from matplotlib.image import imsave
+from matplotlib.pyplot import imshow, savefig, title, tight_layout
 from vizdoom_utils import resize_cv_linear, create_game
 
 # ============================== What is this ========================================
@@ -8,9 +10,10 @@ from vizdoom_utils import resize_cv_linear, create_game
 # ====================================================================================
 
 # config
-is_recording = False
+is_recording = True
+save_state = True
 #res = (256, 144)
-res = (1920, 1080)
+res = (640, 400)
 resize_res = (128, 72)
 
 config_file_path = os.path.join(vzd.scenarios_path, "deathmatch_hugo.cfg")
@@ -21,19 +24,31 @@ config_file_path = os.path.join(vzd.scenarios_path, "deathmatch_hugo.cfg")
 if is_recording:
     if not os.path.isdir("screenshots"):
         os.mkdir("screenshots")
-game = create_game(config_file_path, color=True, depth=False, res=res, visibility=True, asyn=True, spec=True)
+game = create_game(config_file_path, color=True, depth=True, label=True, res=res, visibility=True, asyn=True, spec=True)
+states = []
 
 # play the game as a human (while taking screenshots)
 while True:
     game.new_episode()
     i = 0
+    if is_recording:
+        tight_layout()
     while not game.is_episode_finished():
-        i += 1
-        state = game.get_state()
         if is_recording:
+            i += 1
+            state = game.get_state()
             frame = state.screen_buffer
-            frame_lr = resize_cv_linear(frame, )
-            imsave(f"screenshots/{i}.png", frame.transpose(1,2,0))
-            imsave(f"screenshots/low_res_{i}.png", frame_lr.transpose(1,2,0))
+            frame_depth = state.depth_buffer
+            frame_label = state.labels_buffer
+            if save_state:
+                states.append(state)
+            #frame_lr = resize_cv_linear(frame, resize_res)
+            imsave(f"screenshots/{i}.png", frame)
+            #imsave(f"screenshots/low_res_{i}.png", frame_lr)
+            imsave(f"screenshots/depth_{i}.png", frame_depth)
+            imshow(frame_label, cmap="gist_ncar")
+            title(np.unique(frame_label))
+            savefig(f"screenshots/label_{i}.png")
+            #imsave(f"screenshots/label_{i}.png", frame_label, cmap="gist_ncar")
         game.advance_action()
     print(f"You scored: {game.get_total_reward():.2f}")
