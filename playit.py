a62fd3735815fb9769731c0d1ab41858cd375bfa
Hugo^3 <pondypondo@outlook.com>
2024-08-20 01:59:51 +0100

    QoL

--- playit.py
+++ playit.py
@@ -1,11 +1,13 @@
-import vizdoom as vzd
 import os
 import time
+
 import numpy as np
 from matplotlib.image import imsave
-from matplotlib.pyplot import imshow, savefig, title, tight_layout
-from vizdoom_utils import resize_cv_linear, create_game, RewardTracker
 from vizdoom.vizdoom import GameVariable
+from matplotlib.pyplot import imshow, savefig, title, tight_layout
+
+from scenarios import maps
+from vizdoom_utils import resize_cv_linear, create_game, RewardTracker
 
 # ============================== What is this ========================================
 # A small program that allows human players to play (and record) a scenario themselves
@@ -16,15 +18,28 @@ print_score_board = False
 is_recording = True
 save_state = "recording.pkl"
 #res = (256, 144)
-res = (640, 400)
 res = (1920, 1080)
 resize_res = (128, 72)
+n_bots = 0
+n_games = 1                # -1 for infinite
+delay_after_run = 3
+async_game = False
+map_to_play = "map1w"
+seed = 20508
+# precision_action_buffer = [
+#     [False] * 5 + [True],
+#     [False] * 5 + [True],
+#     [False] * 5 + [True],
+# ]
 
-config_file_path = os.path.join(vzd.scenarios_path, "deathmatch_simple.cfg")
-# config_file_path = os.path.join(vzd.scenarios_path, "bots_deathmatch_2_shotgun.cfg")
-# config_file_path = os.path.join(vzd.scenarios_path, "bots_deathmatch_3.cfg")
-#config_file_path = os.path.join(vzd.scenarios_path, "hanger_test.cfg")
-#config_file_path = os.path.join(vzd.scenarios_path, "deadly_corridor_hugo.cfg")
+# On Windows (not a main focus of ViZDoom), a 3-10 seconds delay is sometimes required
+# to run ViZDoom properly, otherwise ViZDoom game instance is never initialized properly,
+# game window would freeze indefinitely. This bug only affects software renderer, game 
+# logic works fine... Also the delay must be not too short AND NOT TOO LONG (why?).
+# Notes: changing ANY line in this file may lead to the wait value becoming invalid
+wait_for_vizdoom_delay = 6
+
+config_file_path = maps[map_to_play]
 
 # create folder for holding screenshots
 if is_recording:
@@ -46,19 +61,21 @@ add_args = " ".join([
     "+sv_noexit 1"
 ])
 
-game = create_game(config_file_path, color=True, depth=True, label=True, res=res, visibility=True, asyn=False, spec=True, add_args=add_args)
-game.set_doom_map("map01")
+game = create_game(config_file_path, color=True, depth=True, label=True, res=res, visibility=True, asyn=async_game, spec=True, add_args=add_args)
+# game.set_doom_map("map01")
+game.set_seed(seed)
 # game.send_game_command("iddqd")
 game.new_episode()
 states = []
 
-time.sleep(7)
+time.sleep(wait_for_vizdoom_delay)
 try:
     # play the game as a human (while taking screenshots)
-    while True:
+    games_to_play = [1] * n_games if n_games >= 0 else True
+    while games_to_play:
         game.new_episode()
         game.send_game_command("removebots")
-        for _ in range(8):
+        for _ in [0] * n_bots:
             game.send_game_command('addbot')
         #game.send_game_command("idspispopd")
         #game.send_game_command("idbeholdi")
@@ -67,25 +84,26 @@ try:
         r = RewardTracker(game)
         i = 0
         if is_recording:
-            tight_layout()
+            pass
+            # tight_layout()
         while not game.is_episode_finished():
             if is_recording:
                 i += 1
                 state = game.get_state()
-                # frame = state.screen_buffer
+                frame = state.screen_buffer
                 # frame_depth = state.depth_buffer
                 # frame_label = state.labels_buffer
                 # print(state.game_variables)
                 if save_state:
                     states.append(state)
                 # #frame_lr = resize_cv_linear(frame, resize_res)
-                # imsave(f"screenshots/{i}.png", frame)
+                imsave(f"screenshots/{map_to_play}_{i}.png", frame)
                 # #imsave(f"screenshots/low_res_{i}.png", frame_lr)
                 # imsave(f"screenshots/depth_{i}.png", frame_depth)
-                # imshow(frame_label, cmap="gist_ncar")
+                # imshow(frame_label, cmap="jet")
                 # title(np.unique(frame_label))
                 # savefig(f"screenshots/label_{i}.png")
-                # #imsave(f"screenshots/label_{i}.png", frame_label, cmap="gist_ncar")
+                # #imsave(f"screenshots/label_{i}.png", frame_label, cmap="jet")
             if print_score_board:
                 sv_state = game.get_server_state()
                 print(
@@ -98,15 +116,18 @@ try:
                     ], end="\r", flush=True
                 )
             #print(f"\rDmg: {game.get_game_variable(GameVariable.DAMAGECOUNT):.2f}", end="", flush=True)
-            # print(f"\rWeapon: {int(game.get_game_variable(GameVariable.FRAGCOUNT))}", end='')
+            # print(f"\rWeapon: {int(game.get_game_variable(GameVariable.SELECTED_WEAPON))}", end='')
             game.advance_action()
             r.update()
         print(f"You scored: {int(game.get_game_variable(GameVariable.FRAGCOUNT))}")
+        games_to_play.pop()
+        if games_to_play:
+            time.sleep(delay_after_run)
 except Exception as e:
     raise e
 finally:
     import pickle
     if save_state:
         with open(save_state, "wb") as f:
-            pickle.dump(states, f, -1)
+            pickle.dump(states, f, 5)
         print(f"States saved to {save_state}")
