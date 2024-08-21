f506abe68fda4513acaec22fe779f7b0a7cceecf
Hugo^3 <pondypondo@outlook.com>
2024-08-21 14:59:06 +0100

    qol

--- models/ss.py
+++ models/ss.py
@@ -1,16 +1,10 @@
 import gc
-from time import time, sleep
 
 import numpy as np
-import matplotlib.pyplot as plt
-from tqdm.notebook import trange, tqdm
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, random_split
-import torchvision.transforms as transforms
-import torchvision.models.segmentation as models
+from torchvision import models
 
 # from semantic_segmentation.dataset import CustomDataset, ToTensorNormalize
 
@@ -30,11 +24,11 @@ def calculate_iou(pred, target, num_classes):
     return np.nanmean(ious)
 
 # model_cls, model_weight, mobile_name, tqdm_miniters = {
-#     "mobile"    : (models.deeplabv3_mobilenet_v3_large, 
-#                    models.DeepLabV3_MobileNet_V3_Large_Weights.DEFAULT,
+#     "mobile"    : (models.segmentation.deeplabv3_mobilenet_v3_large, 
+#                    models.segmentation.DeepLabV3_MobileNet_V3_Large_Weights.DEFAULT,
 #                    "DeepLabV3-MobileNet", 20),
-#     "res101"    : (models.deeplabv3_resnet101, 
-#                    models.DeepLabV3_ResNet101_Weights.DEFAULT,
+#     "res101"    : (models.segmentation.deeplabv3_resnet101, 
+#                    models.segmentation.DeepLabV3_ResNet101_Weights.DEFAULT,
 #                    "DeepLabV3-ResNet101", 5)
 # }[model_type]
 
@@ -45,10 +39,10 @@ def init_res101(checkpoint: str | dict = "./semantic_segmentation/resnet101/ss_r
                 jit: bool = False, force: bool = False, verbose: int=0):
     global res101
 
-    if force or (not isinstance(res101, models.DeepLabV3)):
+    if force or (not isinstance(res101, models.segmentation.DeepLabV3)):
         if isinstance(checkpoint, str):
             checkpoint = torch.load(checkpoint)["model"]
-        res101 = models.deeplabv3_resnet101(weights=models.DeepLabV3_ResNet101_Weights.DEFAULT)
+        res101 = models.segmentation.deeplabv3_resnet101(weights=models.segmentation.DeepLabV3_ResNet101_Weights.DEFAULT)
 
         prev = res101.classifier[-1]
         res101.classifier[-1] = nn.Conv2d(prev.in_channels, 13, kernel_size=prev.kernel_size, stride=prev.stride)
