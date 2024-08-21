b1ebfb18232a3989e534823e955816bc02cef620
Hugo^3 <pondypondo@outlook.com>
2024-08-21 20:08:25 +0100

    experiments

--- create_run.py
+++ create_run.py
@@ -26,28 +26,30 @@ with open("collect.py") as f:
 # with open(f"collect.{script_ext}", 'w') as f:
 #     f.write(f"{header}\npython collect.py\npause")
 
-for i in range(88, 105+1):
+for i in range(100, 105+1):
     with open(f"collect{i}.py", 'w') as f:
         f.write(content.replace("task_idx = ", f"task_idx = {i-1}\n# task_idx = "))
-    # with open(f"collect{i}.{script_ext}", 'w') as f:
-    #     f.write(f"{header}\npython collect{i}.py\npause")
+    if iswin:
+        with open(f"collect{i}.{script_ext}", 'w') as f:
+            f.write(f"{header}\npython collect{i}.py\npause")
 
-queue = list(range(88, 105+1))
-n_workers = 3
-n_jobs_per_worker = (len(queue) / n_workers).__ceil__()
-i = 0
-special = [] # list(range(72, 63, -1))
-while queue:
-    current_jobs = [] + special
-    special.clear()
-    for _ in [0] * n_jobs_per_worker:
-        if queue:
-            current_jobs.append(queue.pop())
-    job_section = '\n'.join([f"{comment}\npython collect{j}.py" for j in current_jobs])
-    with open(f"collect_batch{i}.{script_ext}", 'w') as f:
-        f.write(f"{header}\n{job_section}\npause")
-    i += 1
-    print(f"Worker {i}: {current_jobs}")
+if True:
+    queue = list(range(100, 105+1))
+    n_workers = 3
+    n_jobs_per_worker = (len(queue) / n_workers).__ceil__()
+    i = 0
+    special = [] # list(range(72, 63, -1))
+    while queue:
+        current_jobs = [] + special
+        special.clear()
+        for _ in [0] * n_jobs_per_worker:
+            if queue:
+                current_jobs.append(queue.pop())
+        job_section = '\n'.join([f"{comment}\npython collect{j}.py" for j in current_jobs])
+        with open(f"collect_batch{i}.{script_ext}", 'w') as f:
+            f.write(f"{header}\n{job_section}\npause")
+        i += 1
+        print(f"Worker {i}: {current_jobs}")
 
 # with open("logs/savefig.py") as f:
 #     content = f.read()
