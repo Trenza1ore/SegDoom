28b11ef32ad24b1d66cb78625472ccceb95a4a8a
Hugo^3 <pondypondo@outlook.com>
2024-08-22 22:46:24 +0100

    Added lfs support, new experiments, enhanced load_merged_records function

--- parse_data/helper_func.py
+++ parse_data/helper_func.py
@@ -4,15 +4,19 @@ import glob
 
 import numpy as np
 from tqdm.rich import tqdm
+from tqdm.notebook import tqdm as tqdm_n
 
-def load_merged_records(path_: str, load_obs: bool=True, load_pos: bool=True, obs_transform: Callable=lambda x: x):
+def load_merged_records(path_: str, load_obs: bool=True, load_pos: bool=True, from_notebook: bool=False,
+                        obs_transform: Callable=lambda x: x) -> tuple[list|np.ndarray, list|np.ndarray, list|np.ndarray, list|np.ndarray, list|np.ndarray]:
     obs, pos, ep_ends, weapon, fps, miou = [], [], [], [], [], []
     ep_offset = 0
     matching_files = glob.glob(path_)
     if len(matching_files) < 1:
         raise FileNotFoundError(f"No match for glob expression: {path_}")
-    for f in tqdm(matching_files):
-        x = np.load(f)
+    tqdm_config = dict(iterable=matching_files, leave=False, desc=path_)
+    tqdm_iter = tqdm_n(**tqdm_config) if from_notebook else tqdm(**tqdm_config)
+    for f in tqdm_iter:
+        x = np.load(f, allow_pickle=True)
         if load_obs:
             obs.append(x["obs"])
         if load_pos:
@@ -27,10 +31,10 @@ def load_merged_records(path_: str, load_obs: bool=True, load_pos: bool=True, ob
         if "miou" in x:
             miou.append(x["miou"])
 
-    if len(miou) == 0 or any(np.issubdtype(arr.dtype, np.integer) for arr in miou):
-        miou = []
-    else:
+    if len(miou) > 0 and all(isinstance(arr, np.ndarray) and np.issubdtype(arr.dtype, np.floating) for arr in miou):
         miou = np.concatenate(miou, axis=0)
+    else:
+        miou = []
     return (np.concatenate(obs, axis=0) if load_obs else [], 
             np.concatenate(pos, axis=0) if load_pos else [],
             np.concatenate(ep_ends, axis=0) if ep_ends else [],
